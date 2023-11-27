421022d1c40ada9720bf1166d190c93457d3758d
leeun <Nhunglee.221@gmail.com>
2023-11-27 21:23:09 +0700

    Update code 27/11/23

---
+++ asset/javascript/library.js
@@ -0,0 +1,10 @@
+// ====NAV BAR MENU MOBILE====
+function openNav() {
+    const headerNav = document.getElementById("mySidenav");
+    headerNav.classList.add('active');
+}
+
+function closeNav() {
+    const headerNav = document.getElementById("mySidenav");
+    headerNav.classList.remove('active');
+}
